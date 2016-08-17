28da36e49d521d653291b01c6eb740e6fca37eda
Xaver Hellauer <software@hellauer.bayern>
2016-08-17 10:58:41 +0200

    Add files via upload

---
+++ .config/notion/default-session--0/cfg_statusd.lua
@@ -0,0 +1,11 @@
+-- Diese Datei wurde durch notion generiert. Nicht editieren.
+return {
+    ["load"] = {
+    },
+    ["mail"] = {
+    },
+    ["date"] = {
+        ["date_format"] = "%a %Y-%m-%d %H:%M",
+    },
+}
+
