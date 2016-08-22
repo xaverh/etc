41c491396872da18b3ad5c4bfadd3fb3f1cfe30d
Xaver Hellauer <software@hellauer.bayern>
2016-08-22 20:16:40 +0200

    -nitrogen

--- bin/install-fedora.sh
+++ bin/install-fedora.sh
@@ -3,7 +3,7 @@ sudo dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-re
 # sudo dnf -y install clipit xfce4-power-manager pnmixer \
 # catfish \
 # gimp transmission libreoffice gmrun arandr \
-# xfce4-notifyd nitrogen scrot gtk-murrine-engine ghc golang \
+# xfce4-notifyd scrot gtk-murrine-engine ghc golang \
 # golang-vim ghc libdvdcss texlive-scheme-medium gvim vim faac flac p7zip hunspell-de \
 # mythes-de wget samba freetype-freeworld  vim obconf lxappearance obmenu \
 # network-manager-applet thunderbird firewall-config slock htop cups \
@@ -11,4 +11,4 @@ sudo dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-re
 # rdesktop claws-mail-plugins clang golang-godoc @c-development
 ### hsetroot cb-compositor cb-exit
 
-sudo dnf install xclip tint2 pcmanfm conky tumbler tumbler-extras zathura-plugins-all flash-plugin unrar gstreamer1-plugins-bad-freeworld gstreamer1-libav gstreamer1-plugins-ugly gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras vlc nitrogen viewnior lxappearance-obconf gtk-murrine-engine gtk-unico-engine network-manager-applet seahorse
+sudo dnf install xclip tint2 pcmanfm conky tumbler tumbler-extras zathura-plugins-all flash-plugin unrar gstreamer1-plugins-bad-freeworld gstreamer1-libav gstreamer1-plugins-ugly gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras vlc viewnior lxappearance-obconf gtk-murrine-engine gtk-unico-engine network-manager-applet seahorse
