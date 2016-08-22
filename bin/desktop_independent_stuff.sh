d9a1dc046489e6cb856007d5ac083628f3531cbd
Xaver Hellauer <xha@Andermatt.fritz.box>
2016-08-22 18:02:14 +0200

    diverse stuff for Fedora

--- bin/desktop_independent_stuff.sh
+++ bin/desktop_independent_stuff.sh
@@ -4,3 +4,6 @@
 if egrep -iq 'touchpad' /proc/bus/input/devices; then
     synclient VertEdgeScroll=0 HorizEdgeScroll=0 TapButton1=1 VertScrollDelta=-15 HorizScrollDelta=-15 2>/dev/null
 fi
+
+## GNOME PolicyKit and Keyring
+eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
