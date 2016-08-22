7149aeaf51e32c64c9bb8aa7f762f93fd1558143
Xaver Hellauer <software@hellauer.bayern>
2016-08-22 12:22:58 +0200

    Set_my_git function; arch packages updated

--- bin/install-arch.sh
+++ bin/install-arch.sh
@@ -1,5 +1,7 @@
 sudo pacman -Syyu --noconfirm
-sudo pacman -Sq --noconfirm wget vlc chromium obconf dmenu i3 unzip git tint2 pcmanfm xarchiver gvfs p7zip unrar zsh rxvt-unicode vim texlive-most clang viewnior lxappearance-obconf gtk-engine-murrine compton slock nitrogen volumeicon pulseaudio conky dunst wmctrl xdotool python2-dbus pygtk
+sudo pacman -Sq --noconfirm wget vlc chromium dmenu i3 unzip git tint2 pcmanfm xarchiver gvfs p7zip unrar zsh rxvt-unicode vim texlive-most clang viewnior lxappearance-obconf gtk-engine-murrine compton slock nitrogen volumeicon pulseaudio conky dunst wmctrl xdotool python2-dbus pygtk nitrogen pcmanfm exfat-utils dbus flashplugin notion dzen2
+
+# sent pnmixer obmixer robot scrot zathura-pdf-poppler
 
 chsh -s /bin/zsh
 
