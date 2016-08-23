f6b91599e858a10b4ea3eb5f60faab9038075961
Xaver Hellauer <software@hellauer.bayern>
2016-08-23 17:47:33 +0200

    minor changes to openbox setup

--- bin/desktop_independent_stuff.sh
+++ bin/desktop_independent_stuff.sh
@@ -5,8 +5,11 @@ if egrep -iq 'touchpad' /proc/bus/input/devices; then
     synclient VertEdgeScroll=0 HorizEdgeScroll=0 TapButton1=1 VertScrollDelta=-15 HorizScrollDelta=-15 2>/dev/null
 fi
 
-## GNOME PolicyKit and Keyring
-eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
+# Gnome Keyring
+eval "`gnome-keyring-daemon -d -s -c pkcs11,ssh,gpg,secrets`"
+export GNOME_KEYRING_CONTROL
+export SSH_AUTH_SOCK
+export GPG_AGENT_INFO
 
 # start rxvt-unicode daemon
 urxvtd &
