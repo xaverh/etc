96075664c2f6de190c2ab6a5f6333ecf7803dc72
Xaver Hellauer <software@hellauer.bayern>
2016-08-26 16:07:19 +0200

    minor cleansing

--- bin/install_manjaro.sh
+++ bin/install_manjaro.sh
@@ -1,2 +1,2 @@
-yaourt -S gconf vim base-devel rxvt-unicode urxvtcd lua jdk8-openjdk dropbox pcmanfm-gtk3 xorg-fontsel viewnior steam-native gparted
+yaourt -S gconf vim base-devel rxvt-unicode urxvtcd lua jdk8-openjdk dropbox pcmanfm-gtk3 xorg-fontsel viewnior steam-native gparted xorg-xkill
 sudo pacman -Rsc lightfirefox
