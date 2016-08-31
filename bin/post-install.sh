bd61fcd070cf44be392b13d97d7aa535efa76a8f
Xaver Hellauer <software@hellauer.bayern>
2016-09-01 01:18:44 +0200

    deactivate fedora & delete openSUSE stuff in post-install.sh

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -2,6 +2,9 @@
 
 # besser: in /etc/X11/Xresources:
 
+sudo pacman -S zsh rxvt-unicode vim texlive-most clang pcmanfm-gtk3 xarchiver otf-fira-mono otf-fira-sans compton lua gparted xorg-xkill steam noto-fonts noto-fonts-cjk noto-fonts-emoji screenfetch xorg-fontsel viewnior gconf jdk8-openjdk dunst pkgfile scrot
+# yaourt -S dropbox spotify gtk-theme-arc super-flat-remix-icon-theme
+
 echo "Xft.dpi: 96
 Xft.antialias: true
 Xft.hinting: true
@@ -9,61 +12,37 @@ Xft.hintstyle: hintslight
 Xft.rgba: rgb
 Xft.lcdfilter: lcddefault
 " | sudo tee /etc/X11/Xresources > /dev/null
+sudo chmod 644 /etc/X11/Xresources
 
 gsettings "set" "org.gnome.settings-daemon.plugins.xsettings" "hinting" "slight"
 gsettings "set" "org.gnome.settings-daemon.plugins.xsettings" "antialiasing" "rgba"
 
-sudo pacman -S gnome-themes-maia firefox-kde-i18n-de zsh rxvt-unicode vim texlive-most kdemultimedia-ffmpegthumbs clang clementine
-yaourt kdegraphics-thumbnailsers-framework-git
-
-# sudo yum-config-manager --add-repo=http://negativo17.org/repos/fedora-handbrake.repo
-
 # Fedora
-sudo dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-`rpm -E %fedora`.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-`rpm -E %fedora`.noarch.rpm http://linuxdownload.adobe.com/adobe-release/adobe-release-`uname -m`-1.0-1.noarch.rpm http://rpm.livna.org/livna-release.rpm
-
-sudo dnf upgrade -y
-
-# GNOME-Shell-Plugins: Dash to dock, Hide top bar, Straight top bar, Transparent top bar
-
-sudo apt-get install adobe-flashplugin git zsh texlive texlive-latex-extra unrar gimp faac faad p7zip fonts-linuxlibertine clang clang-format-3.7 lua5.3 golang xclip network-manager-openconnect-gnome
-
-sudo dnf -y install zsh texlive-scheme-medium vim-enhanced vim-X11 \
-flash-plugin gstreamer1-plugins-bad-freeworld glib2-devel \
-gstreamer1-libav gstreamer1-plugins-ugly \
-gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras \
-gimp faac flac evince-djvu evince-dvi \
-samba  linux-libertine-fonts \
-linux-libertine-biolinum-fonts clang golang golang-godoc \
-freetype-freeworld lato-fonts ghc @c-development @gnome-games \
-transcode kernel-devel @"Development Libraries" glibc-static libstdc++-static \
-libdvdcss fuse-exfat cmake gnome-tweak-tool gnome-kra-ora-thumbnailer \
-gnome-nds-thumbnailer gnome-epub-thumbnailer cabextract lzip \
-p7zip p7zip-plugins unrar libreoffice-langpack-de
-
-# openSUSE
-
-# http://www.opensuse-community.org
-
-sudo zypper install texlive-scheme-full gvim +pattern:devel_java faad2 faac \
-	flac evince-plugin-"*" llvm-clang go go-doc ghc +pattern:devel_C_C++ \
-	transcode
-
-
-# thunderbird vlc  raw-thumbnailer clementine calibre gnome-shell-theme-elegance-colors  gstreamer-plugins-{bad,base,good,ugly,bad-free,good-extras,bad-nonfree,bad-free-extras} numix-icon-theme numix-icon-theme-circle
-
-# sudo zypper install libdvdcss2 texlive-scheme-medium \
-# gstreamer-plugins-good-extra faac flac p7zip linux-libertine-fonts \
-# llvm-clang go go-vim ghc gcc-c++ nautilus-extension-dropbox
-
-# Samba:
-sudo /sbin/setsebool -P samba_enable_home_dirs on
-sudo systemctl enable smb && sudo systemctl enable nmb && sudo systemctl start smb && sudo systemctl start nmb
-# Firewall: samba und samba-client zulassen, 8611/tcp für bjnp
-
+# sudo yum-config-manager --add-repo=http://negativo17.org/repos/fedora-handbrake.repo
+# sudo dnf upgrade -y
+# sudo dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-`rpm -E %fedora`.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-`rpm -E %fedora`.noarch.rpm http://linuxdownload.adobe.com/adobe-release/adobe-release-`uname -m`-1.0-1.noarch.rpm http://rpm.livna.org/livna-release.rpm
+# sudo dnf -y install zsh texlive-scheme-medium vim-enhanced vim-X11 \
+# flash-plugin gstreamer1-plugins-bad-freeworld glib2-devel \
+# gstreamer1-libav gstreamer1-plugins-ugly \
+# gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras \
+# gimp faac flac evince-djvu evince-dvi \
+# samba  linux-libertine-fonts \
+# linux-libertine-biolinum-fonts clang golang golang-godoc \
+# freetype-freeworld lato-fonts ghc @c-development @gnome-games \
+# transcode kernel-devel @"Development Libraries" glibc-static libstdc++-static \
+# libdvdcss fuse-exfat cmake gnome-tweak-tool gnome-kra-ora-thumbnailer \
+# gnome-nds-thumbnailer gnome-epub-thumbnailer cabextract lzip \
+# p7zip p7zip-plugins unrar libreoffice-langpack-de
+# # Samba:
+# sudo /sbin/setsebool -P samba_enable_home_dirs on
+# sudo systemctl enable smb && sudo systemctl enable nmb && sudo systemctl start smb && sudo systemctl start nmb
+# # Firewall: samba und samba-client zulassen, 8611/tcp für bjnp
 # http://rpmfusion.org/Howto/nVidia bzw. akmod-catalyst
 # http://www.if-not-true-then-false.com/2014/fedora-20-nvidia-guide/
 
-chsh -s /bin/zsh
+# GNOME-Shell-Plugins: Dash to dock, Hide top bar, Straight top bar, Transparent top bar
+
+
 
 # http://wiki.ubuntuusers.de/Canon_Pixma_Scanner # braucht Neustart
 
@@ -82,11 +61,12 @@ chsh -s /bin/zsh
 # To make the change permanent, put vm.vfs_cache_pressure=50 on your
 # /etc/sysctl.conf file.
 
-echo -e "default-cache-ttl 18000\nmax-cache-ttl 86400\nignore-cache-for-signing" >> ~/.gnupg/gpg-agent.conf
-echo "use-agent" >>  ~/.gnupg/gpg.conf
+# echo -e "default-cache-ttl 18000\nmax-cache-ttl 86400\nignore-cache-for-signing" >> ~/.gnupg/gpg-agent.conf
+# echo "use-agent" >>  ~/.gnupg/gpg.conf
 
-# Cocos2d-X
-libX11-devel.x86_64 libXmu-devel.x86_64 libXi-devel.x86_64 libzip-devel.x86_64
-libpng12-devel.x86_64 libcurl-devel.x86_64 libsqlite3x-devel.x86_64
-libgle-devel.x86_64 glew-devel glfw-devel fontconfig-devel ant
+# # Cocos2d-X
+# libX11-devel.x86_64 libXmu-devel.x86_64 libXi-devel.x86_64 libzip-devel.x86_64
+# libpng12-devel.x86_64 libcurl-devel.x86_64 libsqlite3x-devel.x86_64
+# libgle-devel.x86_64 glew-devel glfw-devel fontconfig-devel ant
 
+chsh -s /bin/zsh
