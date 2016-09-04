e41f2a1be4f87ca5bd7cf2396899180aa50ebe52
Xaver Hellauer <software@hellauer.bayern>
2016-09-05 01:24:08 +0200

    new packages; new softlinks

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -2,26 +2,20 @@
 
 # Installing stuff
 sudo pacman -Syyu --noconfirm
-sudo pacman -Sq --noconfirm vlc firefox lxappearance-obconf zsh rxvt-unicode vim texlive-most clang pcmanfm-gtk3 xarchiver compton lua gparted xorg-xkill steam noto-fonts noto-fonts-cjk noto-fonts-emoji screenfetch viewnior jdk8-openjdk dunst pkgfile scrot jsoncpp feh xorg-xfontsel wget adobe-source-code-pro-fonts adobe-source-serif-pro-fonts adobe-source-sans-pro-fonts
+sudo pacman -Sq --noconfirm vlc qt4 firefox lxappearance-obconf zsh rxvt-unicode vim texlive-most clang pcmanfm-gtk3 xarchiver compton lua gparted xorg-xkill steam noto-fonts noto-fonts-cjk noto-fonts-emoji screenfetch viewnior jdk8-openjdk dunst pkgfile scrot jsoncpp feh xorg-xfontsel wget adobe-source-code-pro-fonts adobe-source-serif-pro-fonts adobe-source-sans-pro-fonts tint2 dmenu xsreensaver conky ttf-linux-libertine gimp l3afpad zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb
 
-# Install and start dropbox
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-~/.dropbox-dist/dropboxd &!
-
+mkdir ~/Software
 cd ~/Software
-wget -O - "https://aur.archlinux.org/cgit/aur.git/snapshot/spotify.tar.gz" | tar xzf -
-cd spotify
+wget -O - "https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz" | tar xzf -
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+cd cower
+makepkg -si
+cd ~/Software
+cower -d burgaur
+cd burgaur
 makepkg -si
 
-cd ~/Software
-wget -O - "https://aur.archlinux.org/cgit/aur.git/snapshot/sgi-fonts.tar.gz" | tar xzf -
-cd sgi-fonts
-makepkg -si
-
-cd ~/Software
-wget -O - "https://aur.archlinux.org/cgit/aur.git/snapshot/urxvtcd.tar.gz" | tar xzf -
-cd urxvtcd
-makepkg -si
+burgaur -mi spotify sgi-fonts urxvtcd numix-frost-themes conan dropbox
 
 ## setting up antialiasing
 echo "Xft.dpi: 96
@@ -48,7 +42,17 @@ ln -s ~/Software/dotfiles/.zsh ~
 ln -s ~/Software/dotfiles/.zshrc ~
 ln -s ~/Software/dotfiles/.zshenv ~
 ln -s ~/Software/dotfiles/.zprofile ~
-
+rm -r ~/.config/openbox
+ln -s ~/Software/dotfiles/.config/openbox ~/.config/
+ln -s ~/Software/dotfiles/.config/tint2 ~/.config/
+ln -s ~/Software/dotfiles/.local/share/applications ~/.local/share/
+rm -r ~/.config/fontconfig
+ln -s ~/Software/dotfiles/.config/fontconfig ~/.config/
+ln -s ~/Software/dotfiles/.config/dunst ~/.config/
+ln -s ~/Software/dotfiles/.compton.conf ~
+mkdir ~/.fonts
+ln -s ~/Dropbox/Fonts/Menlo ~/.fonts
+ln -s ~/Dropbox/Fonts/San\ Francisco ~/.fonts
 
 # Fedora
 # sudo yum-config-manager --add-repo=http://negativo17.org/repos/fedora-handbrake.repo
