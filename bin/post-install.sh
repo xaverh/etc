d11f48b92554b72317e77de591d2b3ea2c597f87
Xaver Hellauer <software@hellauer.bayern>
2016-09-07 01:42:35 +0200

    -burgaur +pkgbuilder

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -2,20 +2,16 @@
 
 # Installing stuff
 sudo pacman -Syyu --noconfirm
-sudo pacman -Sq --noconfirm vlc qt4 firefox lxappearance-obconf zsh rxvt-unicode vim texlive-most clang pcmanfm-gtk3 xarchiver compton lua gparted xorg-xkill steam noto-fonts noto-fonts-cjk noto-fonts-emoji screenfetch viewnior jdk8-openjdk dunst pkgfile scrot jsoncpp feh xorg-xfontsel wget adobe-source-code-pro-fonts adobe-source-serif-pro-fonts adobe-source-sans-pro-fonts tint2 dmenu xsreensaver conky ttf-linux-libertine gimp l3afpad zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb libstdc++ llvm imagemagick lightdm-gtk-greeter-settings accountsservice unrar obmenu
+sudo pacman -Sq --noconfirm vlc qt4 firefox lxappearance-obconf zsh rxvt-unicode vim texlive-most clang pcmanfm-gtk3 xarchiver compton lua gparted xorg-xkill steam noto-fonts noto-fonts-cjk noto-fonts-emoji screenfetch viewnior jdk8-openjdk dunst pkgfile scrot jsoncpp feh xorg-xfontsel wget adobe-source-code-pro-fonts adobe-source-serif-pro-fonts adobe-source-sans-pro-fonts tint2 dmenu xscreensaver conky ttf-linux-libertine gimp l3afpad zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb libstdc++5 llvm imagemagick lightdm-gtk-greeter-settings accountsservice unrar obmenu
 
 mkdir ~/Software
 cd ~/Software
-wget -O - "https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz" | tar xzf -
-gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-cd cower
-makepkg -si
-cd ~/Software
-cower -d burgaur
-cd burgaur
+wget -O - "https://aur.archlinux.org/cgit/aur.git/snapshot/pkgbuilder.tar.gz" | tar xzf -
+cd pkgbuilder
 makepkg -si
 
-burgaur -mi spotify sgi-fonts urxvtcd numix-frost-themes conan dropbox
+cd ~/Software
+pkgbuilder spotify sgi-fonts urxvtcd numix-frost-themes conan dropbox
 
 ## setting up antialiasing
 echo "Xft.dpi: 96
