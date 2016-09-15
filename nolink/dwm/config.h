a7e0365559e07a01c68c1e811a639c0ac177728c
Xaver Hellauer <software@hellauer.bayern>
2016-09-16 01:41:24 +0200

    switch to thunar, uselessgaps for single floating windows

--- nolink/dwm/config.h
+++ nolink/dwm/config.h
@@ -9,15 +9,14 @@ static const char dmenufont[]       = "HelveticaNeue:size=11";
 static const char normbordercolor[] = "#333333";
 static const char normbgcolor[]     = "#1e1e1e";
 static const char normfgcolor[]     = "#d4d4d4";
-static const char selbordercolor[]  = "#d4d4d4";
-static const char selbgcolor[]      = "#d4d4d4";
-static const char selfgcolor[]      = "#1e1e1e";
+static const char selbordercolor[]  = "#81d8d0";
+static const char selbgcolor[]      = "#333333";
+static const char selfgcolor[]      = "#8b4e86";
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusonwheelscroll = 0;        /* 0 means using the scroll wheel on a window will not change focus */
-static const unsigned int gappx     = 15;       /* gap pixel between windows (part of the uselessgaps patch) */
+static const unsigned int gappx     = 18;       /* gap pixel between windows (part of the uselessgaps patch) */
 
 /* tagging */
 static const char *tags[] = { "🏄", "🏢", "🎸", "📖", "📧", "🍺" };
@@ -62,7 +61,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *dmenucmd[] = {"/home/xha/Software/dotfiles/bin/dmenu_recent", NULL};
 static const char *termcmd[]  = { "urxvtcd", NULL };
-static const char *filemanagercmd[]  = { "pcmanfm", NULL };
+static const char *filemanagercmd[]  = { "thunar", NULL };
 static const char *playpausecmd[] = {"/home/xha/Software/dotfiles/bin/hey_dj.sh","PlayPause", NULL};
 static const char *playnextcmd[] = {"/home/xha/Software/dotfiles/bin/hey_dj.sh","Next", NULL};
 static const char *playpreviouscmd[] = {"/home/xha/Software/dotfiles/bin/hey_dj.sh","Previous", NULL};
