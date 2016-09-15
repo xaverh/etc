23b162f8f9d46251d91f05c38ad891af08b05ea1
Xaver Hellauer <software@hellauer.bayern>
2016-09-15 18:11:16 +0200

    less colors

--- nolink/dwm/src/dwm-6.1/config.def.h
+++ nolink/dwm/src/dwm-6.1/config.def.h
@@ -16,6 +16,9 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
+/* False means using the scroll wheel on a window will not change focus */
+static const Bool focusonwheelscroll = False;
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
