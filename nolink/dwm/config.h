acf942c7e0f613360680b131797d8bd0b74981ae
Xaver Hellauer <software@hellauer.bayern>
2016-09-16 23:00:28 +0200

    Golden ratio for dwm; fixing firefox fullscreen bug

--- nolink/dwm/config.h
+++ nolink/dwm/config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"HelveticaNeue:size=11",
+	"HelveticaNeue:size=10",
 	"NotoEmoji:size=11"
 };
 static const char dmenufont[]       = "HelveticaNeue:size=11";
@@ -10,13 +10,13 @@ static const char normbordercolor[] = "#333333";
 static const char normbgcolor[]     = "#1e1e1e";
 static const char normfgcolor[]     = "#d4d4d4";
 static const char selbordercolor[]  = "#81d8d0";
-static const char selbgcolor[]      = "#333333";
+static const char selbgcolor[]      = "#1e1e1e";
 static const char selfgcolor[]      = "#8b4e86";
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const unsigned int gappx     = 18;       /* gap pixel between windows (part of the uselessgaps patch) */
+static const unsigned int gappx     = 20;       /* gap pixel between windows (part of the uselessgaps patch) */
 
 /* tagging */
 static const char *tags[] = { "🏄", "🏢", "🎸", "📖", "📧", "🍺" };
@@ -29,14 +29,13 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Code",     NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Zathura",  NULL,       NULL,       1 << 3,       1,           -1 },
-	{ "Vlc",      NULL,       NULL,       1 << 5,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 0,       0,           -1 }
 };
 
 /* layout(s) */
-static const float mfact     = 0.55f; /* factor of master area size [0.05f..0.95f] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact     = 0.618034f; /* factor of master area size [0.05f..0.95f] */
+static const int nmaster     = 1;      /* number of clients in master area */
+static const int resizehints = 1;      /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
