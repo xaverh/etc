aaf113d6b6a5fa84c445b7a201375922ea6610cc
Xaver Hellauer <xaver@hellauer.bayern>
2017-02-22 15:33:58 +0100

    Update to install guidelines

--- bin/install.sh
+++ bin/install.sh
@@ -6,20 +6,19 @@ timedatectl set-ntp true
 # EFI
 # don't forget the EFI partition
 # https://wiki.archlinux.org/index.php/EFI_System_Partition
-cgdisk /dev/sdX
+gdisk /dev/sdX
 
 # BIOS
 # check which filesystems syslinux can handle
-cfdisk /dev/sdX
+fdisk /dev/sdX
 
 # make filesystems
 # mount everything accordingly below /mnt
 
 vi /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel vim
 
-genfstab -U /mnt >> /mnt/etc/fstab
-vi /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab # settings for SSDs?
 arch-chroot /mnt /bin/bash
 
 passwd
@@ -33,16 +32,21 @@ syslinux-install_update -i -m -a
 vi /boot/syslinux/syslinux.cfg
 
 # UEFI
-bootctl --path=/boot install
+bootctl install
 
-cp /usr/share/systemd/bootctl/loader.conf /boot/loader.conf
-cp /usr/share/systemd/bootctl/arch.conf /boot/loader/entries/
-# get UUID for root=UUID=<uuid of /> and resume=UUID=<uuid of swap>, it put in arch.conf, and then tidy that mess!
-ls -l /dev/disk/by-uuid/ >> /boot/loader/entries/arch.conf
-vi /boot/loader/entries/arch.conf
+# /boot/loader/entries/arch.conf
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux.img
+options root=PARTUUID=f69907aa-e58a-4f56-8bc0-208e5f1ad73a rootfstype=xfs add_efi_memmap rw
+# :r! blkid to get real PARTUUID
 
-vi /etc/mkinitcpio.conf
-# replace "udev" with "systemd"
+# /boot/loader/loader.conf
+timeout 3
+default arch
+
+# replace "udev" with "systemd" in /etc/mkinitcpio.conf
 
 mkinitcpio -p linux
 
@@ -52,40 +56,39 @@ exit
 
 reboot
 
-# Time configuration
-vi /etc/systemd/timesyncd.conf
-timedatectl set-ntp true
-timedatectl set-local-rtc false
-timedatectl set-timezone Europe/Berlin
-
 # Locale configuration
-# This doesn't work yet, we are still missing LC_ALL and LANGUAGE
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+localectl list-locales
+locale-gen
 localectl set-locale LANG=en_US.UTF-8
 localectl set-keymap de
 localectl set-x11-keymap de
 
+# Time configuration
+vi /etc/systemd/timesyncd.conf # TODO
+timedatectl set-ntp true
+timedatectl set-local-rtc false # except on dual boot with Windows
+timedatectl set-timezone Europe/Berlin
+
 # network configuration
 hostnamectl set-hostname myhostname
-systemctl enable --now systemd-networkd
-
-ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 
 echo "[Match]
-Name=eth*
+Name=en*
 [Network]
 DHCP=yes
 [DHCP]
 RouteMetric=10" > /etc/systemd/network/wired.network
 
 echo "[Match]
-Name=wlan*
+Name=wl*
 [Network]
 DHCP=yes
 [DHCP]
 RouteMetric=20" > /etc/systemd/network/wireless.network
 
 ln -sf /run/systemd/resolve/resolv.conf /etc
-/etc/systemd/resolved.conf
+vim /etc/systemd/resolved.conf
 systemctl enable --now systemd-resolved
 
 # Install optional packages
@@ -98,25 +101,30 @@ vi /etc/pacman.conf
 # add ILoveCandy
 pacman -Syu
 # find video card
-# Intel: pacman -S xf86-video-intel mesa-libgl lib32-mesa-libgl vulkan-intel
 lspci | grep -e VGA -e 3D
+# Intel: pacman -S xf86-video-intel mesa-libgl lib32-mesa-libgl vulkan-intel
+# NVidia: XXX
 
-pacman --needed -S  zsh rxvt-unicode vim clang lua noto-fonts{,-cjk,-emoji} screenfetch dunst scrot jsoncpp feh wget adobe-source-{code,sans,serif}-pro-fonts ttf-linux-libertine gimp zathura-{pdf-poppler,ps,djvu,cb} libstdc++5 llvm imagemagick unrar slock git abs mpd ncmpcpp unzip ttyload exfat-utils mpv youtube-dl numlockx npm nodejs mpc p7zip ranger xorg{,-apps,-fonts} firefox steam steam-native-runtime gst-plugins-good gst-libav openmp texlive-most lib32-libpulse lib32-openal lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-gconf lib32-dbus-glib lib32-libnm-glib lib32-alsa-plugins pulseaudio pulseaudio-alsa pamixer alsa-utils bc aria2 lxappearance compton w3m mac cmatrix lolcat ttf-dejavu iperf3
+pacman --needed -S zsh rxvt-unicode clang lua noto-fonts{,-cjk,-emoji} screenfetch dunst scrot feh wget adobe-source-{code,sans,serif}-pro-fonts ttf-linux-libertine gimp zathura-{pdf-poppler,ps,djvu,cb} llvm imagemagick unrar slock git abs unzip ttyload exfat-utils mpv youtube-dl numlockx npm nodejs p7zip xorg{,-apps,-fonts,-xinit} gst-plugins-good gst-libav openmp texlive-most pulseaudio pulseaudio-alsa pamixer alsa-utils bc mac ttf-dejavu openssh xclip ssh-askpass
+
+pacman --needed -S jsoncpp libstdc++5 mpd ncmpcpp mpc ranger firefox steam steam-native-runtime lib32-libpulse lib32-openal lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-gconf lib32-dbus-glib lib32-libnm-glib lib32-alsa-plugins aria2 lxappearance compton w3m cmatrix lolcat iperf3
 
 # Laptop?
-pacman -Sq --noconfirm --needed xf86-input-synaptics acpi wpa_supplicant iw
-ln -s /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bak
-chmod a+r /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+pacman --needed -S xf86-input-synaptics acpi wpa_supplicant iw
+
+# XXX
+# ln -s /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+vim /etc/wpa_supplicant/wpa_supplicant.conf
+chmod a+r /etc/wpa_supplicant/wpa_supplicant.conf
 # ctrl_interface=/var/run/wpa_supplicant
 # eapol_version=1
 # ap_scan=1
 # fast_reauth=1
-systemctl enable --now wpa_supplicant@wlan0
+systemctl enable --now wpa_supplicant@wlan0 ## XXX
 # wpa_passphrase
 
 # DVD?
-pacman -Sq --noconfirm --needed libdvdcss dvd+rw-tools
+pacman --needed -S libdvdcss dvd+rw-tools
 
 # SSD?
 systemctl enable fstrim.timer
