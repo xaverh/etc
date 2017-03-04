c455b57fe8bff9d88ec17f7da905ee3e756ff5d3
Xaver Hellauer <xaver@hellauer.bayern>
2017-03-04 23:53:18 +0100

    formatting

--- bin/install.sh
+++ bin/install.sh
@@ -113,7 +113,10 @@ nodejs p7zip xorg{,-apps,-fonts,-xinit} gst-plugins-good gst-libav openmp \
 texlive-most pulseaudio pulseaudio-alsa pamixer alsa-utils bc mac ttf-dejavu \
 openssh xclip x11-ssh-askpass go go-tools tmux vifm
 
-pacman --needed -S jsoncpp libstdc++5 mpd ncmpcpp mpc ranger firefox steam steam-native-runtime lib32-libpulse lib32-openal lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-gconf lib32-dbus-glib lib32-libnm-glib lib32-alsa-plugins aria2 lxappearance compton w3m cmatrix lolcat iperf3
+pacman --needed -S jsoncpp libstdc++5 mpd ncmpcpp mpc ranger firefox steam \
+steam-native-runtime lib32-libpulse lib32-openal lib32-nss lib32-gtk2 \
+lib32-gtk3 lib32-libcanberra lib32-gconf lib32-dbus-glib lib32-libnm-glib \
+lib32-alsa-plugins aria2 lxappearance compton w3m cmatrix lolcat iperf3
 
 # Laptop?
 pacman --needed -S acpi wpa_supplicant iw
