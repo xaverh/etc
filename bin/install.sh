7034997b3b0bbbaec4415de79e2760c9dbccf8c2
Xaver Hellauer <xaver@hellauer.bayern>
2017-03-04 23:25:53 +0100

    x11-ssh-askpass

--- bin/install.sh
+++ bin/install.sh
@@ -111,7 +111,7 @@ ttf-linux-libertine gimp zathura-{pdf-poppler,ps,djvu,cb} llvm imagemagick \
 unrar slock git abs unzip ttyload exfat-utils mpv youtube-dl numlockx npm \
 nodejs p7zip xorg{,-apps,-fonts,-xinit} gst-plugins-good gst-libav openmp \
 texlive-most pulseaudio pulseaudio-alsa pamixer alsa-utils bc mac ttf-dejavu \
-openssh xclip ssh-askpass go go-tools tmux
+openssh xclip x11-ssh-askpass go go-tools tmux vifm
 
 pacman --needed -S jsoncpp libstdc++5 mpd ncmpcpp mpc ranger firefox steam steam-native-runtime lib32-libpulse lib32-openal lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-gconf lib32-dbus-glib lib32-libnm-glib lib32-alsa-plugins aria2 lxappearance compton w3m cmatrix lolcat iperf3
 
