40258544282304e5afa352ef25af28b7cd38f05c
Xaver Hellauer <xaver@hellauer.bayern>
2018-04-27 20:17:08 +0200

    .eslint.js

---
+++ eslint/.eslint.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: "module",
+    parser: "babel-eslint"
+  },
+  env: {
+    browser: true
+  },
+  extends: ["prettier", "prettier/standard", "plugin:vue/recommended"],
+  plugins: ["vue", "prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
