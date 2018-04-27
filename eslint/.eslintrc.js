9f31b507d85a36c74708729ea0f37c0a41f98e26
Xaver Hellauer <xaver@hellauer.bayern>
2018-04-27 20:48:02 +0200

    Prettier goes out

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,15 +1,4 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    sourceType: "module",
-    parser: "babel-eslint"
-  },
-  env: {
-    browser: true
-  },
-  extends: ["prettier", "prettier/standard", "plugin:vue/recommended"],
-  plugins: ["vue", "prettier"],
-  rules: {
-    "prettier/prettier": "error"
-  }
-};
+  extends: ['standard']
+}
