4b785ac48b92063f6f45008367369871c7dfe9c8
Xaver Hellauer <xaver@hellauer.bayern>
2018-10-23 20:44:48 +0200

    Update post-install.sh

--- nolink/post-install.sh
+++ nolink/post-install.sh
@@ -21,7 +21,9 @@ makepkg -Ccsfi
 
 yay -Syu
 
-yay -S spotify ffmpeg-compat-54 sgi-fonts conan toilet google-chrome-beta wire-desktop-bin visual-studio-code-bin clipmenu openvpn-update-systemd-resolved mons ttf-ibm-plex
+yay -S spotify conan google-chrome wire-desktop-bin visual-studio-code-bin clipmenu openvpn-update-systemd-resolved mons ttf-ibm-plex
+# removed packages
+# ffmpeg-compat-54 toilet sgi-fonts
 
 cd ~/src
 git clone git@github.com:xaverh/dwm.git
