47c71e68d86ea487ee695ae4b4d556518b19f376
Xaver Hellauer <xaver@hellauer.bayern>
2019-03-15 18:34:38 +0100

    MVP bspwmstatusbar

--- bin/bspwmstatus.go
+++ bin/bspwmstatus.go
@@ -35,6 +35,52 @@ const (
 
 	// Formatting
 	alignRight = "%{r}"
+	alignLeft  = "%{l}"
+
+	colorK = "#1E1E1E" /* Grey 10%, R=30, G=30, B=30 */
+	colorR = "#E32791" /* Deep Cerise, R=227, G=39, B=145 */
+	colorG = "#30C798" /* Shamrock, R=48, G=199, B=152 */
+	colorY = "#E3C472" /* Chenin, R=227, G=196, B=114 */
+	colorB = "#6796E6" /* Cornflower Blue, R=103, G=150, B=230 */
+	colorM = "#E59FDF" /* Plum, R=229, G=159, B=223 */
+	colorC = "#81D8D0" /* Riptide, R=129, G=216, B=208 */
+	colorW = "#969696" /* Grey 60%, R=150, G=150, B=150 */
+
+	colorLightK = "#515151" /* Grey 30%, R=81, G=81, B=81 */
+	colorLightR = "#E466AD" /* Hot Pink, R=228, G=102, B=173 */
+	colorLightG = "#6CD1B2" /* Medium Aquamarine, R=108, G=209, B=178 */
+	colorLightY = "#E4CF98" /* Double Colonial White, R=228, G=207, B=152 */
+	colorLightB = "#91B0E6" /* Jordy Blue, R=145, G=181, B=230 */
+	colorLightM = "#E5B6E1" /* French Lilac, R=229, G=182, B=225 */
+	colorLightC = "#A2DCD7" /* Sinbad, R=162, G=220, B=215 */
+	colorLightW = "#E5E6E6" /* Grey 90%, R=229, G=230, B=230 */
+
+	colorDefaultFg         = colorLightW
+	colorDefaultBg         = colorK
+	colorMonitorFg         = "#8dbcdf"
+	colorMonitorBg         = colorDefaultBg
+	colorFocusedMonitorFg  = "#b1d0e8"
+	colorFocusedMonitorBg  = "#144b6c"
+	colorFreeFg            = colorDefaultFg
+	colorFreeBg            = colorDefaultBg
+	colorFreeUl            = colorFreeBg
+	colorFocusedFreeFg     = colorDefaultFg
+	colorFocusedFreeBg     = colorDefaultBg
+	colorFocusedFreeUl     = colorFocusedFreeBg
+	colorOccupiedFg        = "#a7a5a5"
+	colorOccupiedBg        = colorDefaultBg
+	colorOccupiedUl        = colorOccupiedBg
+	colorFocusedOccupiedFg = "#d6d3d2"
+	colorFocusedOccupiedBg = "#504e4e"
+	colorFocusedOccupiedUl = colorFocusedOccupiedBg
+	colorUrgentFg          = colorLightW
+	colorUrgentBg          = colorR
+	colorUrgentUl          = colorUrgentBg
+	colorFocusedUrgentFg   = "#501d1f"
+	colorFocusedUrgentBg   = "#d5443e"
+	colorFocusedUrgentUl   = colorFocusedUrgentBg
+	colorStateFg           = colorM
+	colorStateBg           = colorDefaultBg
 )
 
 var (
@@ -48,6 +94,107 @@ var (
 	srcIPRegex = regexp.MustCompilePOSIX(srcIPPattern)
 	ssidRegex  = regexp.MustCompile("SSID: (.*?)\n")
 )
+
+func formatBspwmStatus(input string) string {
+	items := strings.SplitAfterN(strings.TrimPrefix(input, "W"), ":", -1)
+	result := alignLeft
+	fg := colorDefaultFg
+	bg := colorDefaultBg
+	ul := colorDefaultBg
+	onFocusedMonitor := false
+	monitorCount := 0
+	for _, v := range items {
+		switch v[:1] {
+		case "m":
+			fg = colorMonitorFg
+			bg = colorMonitorBg
+			onFocusedMonitor = false
+			monitorCount++
+
+		case "M":
+			fg = colorFocusedMonitorFg
+			bg = colorFocusedMonitorBg
+			onFocusedMonitor = true
+			monitorCount++
+
+		case "f":
+			fg = colorFreeFg
+			bg = colorFreeBg
+			ul = colorFreeUl
+
+		case "F":
+			if onFocusedMonitor {
+				fg = colorFocusedFreeFg
+				bg = colorFocusedFreeBg
+				ul = colorFocusedFreeUl
+			} else {
+				fg = colorFreeFg
+				bg = colorFreeBg
+				ul = colorFreeUl
+			}
+
+		case "o":
+			fg = colorOccupiedFg
+			bg = colorOccupiedBg
+			ul = colorOccupiedUl
+
+		case "O":
+			if onFocusedMonitor {
+				fg = colorFocusedOccupiedFg
+				bg = colorFocusedOccupiedBg
+				ul = colorFocusedOccupiedUl
+			} else {
+				fg = colorOccupiedFg
+				bg = colorOccupiedBg
+				ul = colorOccupiedUl
+			}
+
+		case "u":
+			fg = colorUrgentFg
+			bg = colorUrgentBg
+			ul = colorUrgentUl
+
+		case "U":
+			if onFocusedMonitor {
+				fg = colorFocusedUrgentFg
+				bg = colorFocusedUrgentBg
+				ul = colorFocusedUrgentUl
+			} else {
+				fg = colorUrgentFg
+				bg = colorUrgentBg
+				ul = colorUrgentUl
+			}
+		}
+		name := strings.TrimSuffix(v[1:], ":")
+		switch v[:1] {
+		case "m":
+			fallthrough
+		case "M":
+			if monitorCount > 1 {
+				result = result + "%{F" + fg + "}%{B" + bg + "}%{A:bspc monitor -f " + name + ":} " + name + " %{A}%{B-}%{F-}"
+			}
+		case "f":
+			fallthrough
+		case "F":
+			fallthrough
+		case "o":
+			fallthrough
+		case "O":
+			fallthrough
+		case "u":
+			fallthrough
+		case "U":
+			result = result + "%{F" + fg + "}%{B" + bg + "}%{U" + ul + "}%{+u}%{A:bspc desktop -f " + name + ":} " + name + " %{A}%{B-}%{F-}%{-u}"
+		case "L":
+			fallthrough
+		case "T":
+			fallthrough
+		case "G":
+			result = result + "%{F" + colorStateFg + "}%{B" + colorStateBg + "} " + name + " %{B-}%{F-}"
+		}
+	}
+	return result
+}
 
 func fixed(pre string, rate int) string {
 	if rate < 0 {
@@ -157,7 +304,7 @@ func updatePower(powChan chan<- string) {
 			icon = pluggedSign
 		}
 
-		powChan <- fmt.Sprintf("%d%%%s", enPerc, icon)
+		powChan <- fmt.Sprintf("%d%%s", enPerc, icon)
 		time.Sleep(time.Duration(10 * time.Second))
 	}
 }
@@ -254,7 +401,7 @@ func updateBspwmState(bspwmChan chan<- string) {
 
 	defer cmd.Wait()
 	for scanner.Scan() {
-		bspwmChan <- fmt.Sprintf(scanner.Text())
+		bspwmChan <- fmt.Sprint(formatBspwmStatus(scanner.Text()))
 	}
 	if err := scanner.Err(); err != nil {
 		bspwmChan <- fmt.Sprintf("reading standard input: %v", err)
