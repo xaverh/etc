5cadcd54222a30693e72c74b2b493d9709f3e551
Xaver Hellauer <xaver@hellauer.bayern>
2019-03-23 23:36:36 +0100

    openSUSE -> Fedora

--- .config/herbstluftwm/panel.sh
+++ .config/herbstluftwm/panel.sh
@@ -17,15 +17,18 @@ fi
 x=${geometry[0]}
 y=${geometry[1]}
 panel_width=${geometry[2]}
-panel_height=22
+panel_height=23
 font="IBM Plex Sans:style=Medium,Regular:pixelsize=15:antialias=true:hintstyle=1:lcdfilter=1:rgba=1"
-# font="-sgi-screen-medium-r-normal--14-140-72-72-m-70-iso8859-1"
-# font="-sgi-screen-medium-r-normal--16-160-72-72-m-80-iso8859-1"
+font2="IBM Plex Sans Hebrew Medium:style=Medium,Regular:pixelsize=15:antialias=true:hintstyle=1:lcdfilter=1:rgba=1"
+font3="IBM Plex Thai Medium:style=Medium,Regular:pixelsize=15:antialias=true:hintstyle=1:lcdfilter=1:rgba=1"
+font4="思源黑体 Medium:style=Medium,Regular:pixelsize=14:antialias=true:hintstyle=1:lcdfilter=1:rgba=1"
+font5=".SF NS Text:style=Medium:pixelsize=14:antialias=true:hintstyle=1:lcdfilter=1:rgba=1"
 bgcolor=$(hc get frame_border_normal_color)
 selbg=$(hc get window_border_active_color)
+
 selfg='#101010'
 
 hc pad $monitor $panel_height
 
 # $HOME/src/jigglyroom/jigglyroom "$monitor" | dzen2 -w $panel_width -x $x -y $y -fn "$font" -h $panel_height  -e "button3=;button4=exec:herbstclient use_index -1;button5=exec:herbstclient use_index +1" -ta l -bg "#1e1e1e" -fg "#e5e6e6"
-$HOME/src/jigglyroom/jigglyroom "$monitor" | $HOME/src/bar/lemonbar -a 32 -u 2 -n herbstluft_panel -g x$panel_height -f "$font" -F "#F5F6F6" -B "#1E1E1E" | sh &
+$HOME/src/jigglyroom/jigglyroom "$monitor" | lemonbar -a 32 -u 2 -n herbstluft_panel -g x$panel_height  -f "$font" -f "$font2" -f "$font3" -f "$font4" -f "$font5" -F "#F5F6F6" -B "#1E1E1E" | sh &
