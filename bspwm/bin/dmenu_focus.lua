56cb974f4678cca0740ef4935f5d722b56f8eb73
Xaver Hellauer <xaver@hellauer.bayern>
2019-04-20 01:43:42 +0200

    use environment variables in dmenu_focus and panel

--- bspwm/bin/dmenu_focus.lua
+++ bspwm/bin/dmenu_focus.lua
@@ -1,5 +1,9 @@
 #!/usr/bin/lua
 
+local panel_fg_color = os.getenv("PANEL_FG_COLOR") or "#424242"
+local panel_bg_color = os.getenv("PANEL_BG_COLOR") or "#F9F8F4"
+local font = os.getenv("PANEL_XFT_FONT") or "SGI Screen:pixelsize=14"
+
 local query = io.popen("bspc query -N -n .window")
 local ids = {}
 for line in query:lines("l") do
@@ -20,7 +24,7 @@ local dmenu_input = "/bin/sh -c 'echo \""
 for k, v in pairs(ids) do
 	dmenu_input = dmenu_input .. k .. '\n'
 end
-dmenu_input = dmenu_input .. "\" | dmenu -l 10 -p \"Focus:\"'"
+dmenu_input = dmenu_input .. "\" | dmenu -i -f -l 10 -p \"Focus:\" -fn \"" .. font .."\" -nb \"" .. panel_bg_color .. "\" -nf \"" .. panel_fg_color .. "\"'"
 print(dmenu_input)
 
 local dmenu = io.popen(dmenu_input)
