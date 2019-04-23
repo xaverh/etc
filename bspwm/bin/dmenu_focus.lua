745193521daf56289862ca8ed6a84398a2848750
Xaver Hellauer <xaver@hellauer.bayern>
2019-04-23 14:55:42 +0200

    dmenu_focus.lua has a swap mode now

--- bspwm/bin/dmenu_focus.lua
+++ bspwm/bin/dmenu_focus.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/lua
 
+local mode = {arg = arg[1] or "--focus", name = string.sub(arg[1],3)}
+
 local panel_fg_color = os.getenv("PANEL_FG_COLOR") or "#424242"
 local panel_bg_color = os.getenv("PANEL_BG_COLOR") or "#F9F8F4"
 local font = os.getenv("PANEL_XFT_FONT") or "SGI Screen:pixelsize=14"
@@ -24,10 +26,10 @@ local dmenu_input = "/bin/sh -c 'echo \""
 for k, v in pairs(ids) do
 	dmenu_input = dmenu_input .. k .. '\n'
 end
-dmenu_input = dmenu_input .. "\" | dmenu -i -f -l 10 -p \"Focus:\" -fn \"" .. font .."\" -nb \"" .. panel_bg_color .. "\" -nf \"" .. panel_fg_color .. "\"'"
+dmenu_input = dmenu_input .. "\" | dmenu -i -f -l 10 -p \"" .. mode.name .. "\" -fn \"" .. font .."\" -nb \"" .. panel_bg_color .. "\" -nf \"" .. panel_fg_color .. "\"'"
 print(dmenu_input)
 
 local dmenu = io.popen(dmenu_input)
 local window_title = dmenu:read("l")
 dmenu:close()
-os.execute("bspc node --focus " .. ids[window_title])
+os.execute("bspc node " .. mode.arg .. " " .. ids[window_title])
