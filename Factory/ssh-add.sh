4f2977bc23053a361709ac7d6f7b105631e86e1c
Xaver Hellauer <xaver@hellauer.bayern>
2019-08-12 20:03:19 +0200

    Delete ssh-add.sh

--- Factory/ssh-add.sh
+++
@@ -1,9 +0,0 @@
-if test -f /usr/lib/ssh/x11-ssh-askpass
-then
-	SSH_ASKPASS=/usr/lib/ssh/x11-ssh-askpass ssh-add < /dev/null
-fi
-
-if test -f /usr/libexec/openssh/x11-ssh-askpass
-then
- 	SSH_ASKPASS=/usr/libexec/openssh/x11-ssh-askpass ssh-add < /dev/null
-fi
