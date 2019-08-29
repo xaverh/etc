cdab7ae59a141f3dc3093500389f8cd1d481b5bc
Xaver Hellauer <xaver@hellauer.bayern>
2019-08-29 20:11:43 +0200

    fix backdrop menu, new commands for window moving and scrolling

--- Factory/change_wallpaper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-xsetroot -bitmap $HOME/etc/backdrops/`ls $HOME/etc/backdrops | shuf -n 1 | tr -d '\n' | tee -a /tmp/wallpaper` `printf -- " -fg #%06x -bg #%06x\n" $(shuf -i0-16777215 -n2) | tee -a /tmp/wallpaper`
