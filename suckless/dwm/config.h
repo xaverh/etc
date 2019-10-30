8d820c1390be5d15f7db7daaf3f90324fe410896
Xaver Hellauer <xaver@hellauer.bayern>
2019-10-30 11:10:01 +0100

    cwm like groups for dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,14 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int snap = 8;     /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const char* fonts[] = {"sans-serif:size=11"};
 #define COL_GRAY1 "#1e1e1e"
 static const char col_gray1[] = COL_GRAY1;
-// static const char col_gray2[] = "#424242";
+static const char col_gray2[] = "#424242";
 #define COL_GRAY3 "#b8b8b8"
 static const char col_gray3[] = COL_GRAY3;
 static const char col_gray4[] = "#e5e6e6";
@@ -16,12 +16,23 @@ static const char col_cyan[] = "#005577";
 static const char col_cerise[] = "#e32791";
 static const char* colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray1},
+    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
     [SchemeSel] = {col_gray4, col_cyan, col_cerise},
 };
 
 /* tagging */
-static const char* tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char* tags[] = {"0", "1", "2",  "3",  "4",
+                             "5", "6", "¬7", "¬8", "¬9"};
+#define HOME_TAGS        0x1U
+#define NONTOGGABLE_TAGS 0x7FU
+#define BROWSERTAG       2U
+#define CODINGTAG        4U
+#define PDFTAG           16U
+#define CHATTAG          32U
+#define MOVIETAG         128U
+#define DUMPSTERTAG      256U
+#define MUSICTAG         256U
+#define JOURNALTAG       512U
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -29,21 +40,27 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class  instance  title  tags mask  isfloating monitor */
-    // { "Code",                       NULL,       NULL,         1 << 1, 0, -1
-    // },
-    {"discord", NULL, NULL, 1 << 6, 0, -1},
     {"Firefox", "Places", "Library", 0, 1, -1},
-    /*	{ "mpv",              NULL,         NULL,         0xFFFF,       0, -1
-       },*/
-    {NULL, "journalctl", NULL, 1 << 8, 0, -1},
-    {"lxqt-openssh-askpass", NULL, NULL, 0, 1, -1},
-    {"mpv", "FM0", NULL, 1 << 7, 0, -1},
-    {NULL, NULL, "Picture in picture", (1 << 8) - 1, 0, -1},
     {"presenter", "sent", "sent", 0, 1, -1},
-    {"Spotify", NULL, NULL, 1 << 5, 0, -1},
-    {"strawberry", NULL, NULL, 1 << 5, 0, -1},
-    {"Steam", NULL, NULL, 1 << 7, 0, -1},
-    {"TelegramDesktop", NULL, NULL, 1 << 6, 0, -1}};
+    {"Code", NULL, NULL, CODINGTAG, 0, -1},
+    {"discord", NULL, NULL, CHATTAG, 0, -1},
+    {"Firefox", NULL, NULL, BROWSERTAG, 0, -1},
+    {"mpv", "gl", NULL, MOVIETAG, 0, -1},
+    {NULL, "journalctl", NULL, JOURNALTAG, 0, -1},
+    {"mpv", "FM0", NULL, MUSICTAG, 0, -1},
+    {NULL, NULL, "MuPDF:", PDFTAG, 0, -1},
+    {NULL, NULL, ".epub -", PDFTAG, 0, -1},
+    {NULL, NULL, ".pdf -", PDFTAG, 0, -1},
+    {NULL, NULL, ".cbz -", PDFTAG, 0, -1},
+    {NULL, NULL, ".EPUB -", PDFTAG, 0, -1},
+    {NULL, NULL, ".PDF -", PDFTAG, 0, -1},
+    {NULL, NULL, ".CBZ -", PDFTAG, 0, -1},
+    {NULL, NULL, "Picture in picture", MOVIETAG, 0, -1},
+    {"Spotify", NULL, NULL, MUSICTAG, 0, -1},
+    {"strawberry", NULL, NULL, MUSICTAG, 0, -1},
+    {"Steam", NULL, NULL, DUMPSTERTAG, 0, -1},
+    {"TelegramDesktop", NULL, NULL, CHATTAG, 0, -1},
+    {"Vivaldi-stable", NULL, NULL, BROWSERTAG, 0, -1}};
 
 /* layout(s) */
 #define DEFAULT_MFACT 0.5688140392f
@@ -60,19 +77,21 @@ static const Layout layouts[] = {
     {"><>", NULL}, /* no layout function means floating behavior */
 };
 
-void focusmaster();
+// void focusmaster();
+void viewortoggleview(const Arg* arg);
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                                      \
-	{MODKEY, KEY, view, {.ui = 1 << TAG}},                                 \
-	    {MODKEY, KEY, focusmaster, {0}},                                   \
-	    {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},         \
+	{MODKEY, KEY, viewortoggleview, {.ui = 1 << TAG}},                     \
+	    {MODKEY | ControlMask, KEY, view, {.ui = 1 << TAG}},               \
 	    {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                  \
-	    {MODKEY | ControlMask | ShiftMask,                                 \
-	     KEY,                                                              \
-	     toggletag,                                                        \
-	     {.ui = 1 << TAG}},
+	{                                                                      \
+		MODKEY | ControlMask | ShiftMask, KEY, toggletag,              \
+		{                                                              \
+			.ui = 1 << TAG                                         \
+		}                                                              \
+	}
 
 #define SHELL "/bin/zsh"
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
@@ -89,50 +108,49 @@ static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char* dmenucmd[] = {"rofi", "-show", "run", NULL};
 static const char* termcmd[] = {"kitty", "-1", NULL};
-static const char* nnncmd[] = {"kitty", "-1", "nnn", NULL};
-static const char* alttermcmd[] = {"kitty",
-                                   "-1",
-                                   "--instance-group",
-                                   "Ysgrifennwr",
-                                   "--override",
-                                   "foreground=#424242",
-                                   "--override",
-                                   "background=#f9f8f4",
-                                   "--override",
-                                   "color0=#f9f8f4",
-                                   "--override",
-                                   "color1=#e32791",
-                                   "--override",
-                                   "color2=#488432",
-                                   "--override",
-                                   "color3=#a25d0e",
-                                   "--override",
-                                   "color4=#2c65b5",
-                                   "--override",
-                                   "color5=#B062A7",
-                                   "--override",
-                                   "color6=#27BBBE",
-                                   "--override",
-                                   "color7=#999999",
-                                   "--override",
-                                   "color8=#B8B8B8",
-                                   "--override",
-                                   "color9=#9F1B66",
-                                   "--override",
-                                   "color10=#325D23",
-                                   "--override",
-                                   "color11=#71410A",
-                                   "--override",
-                                   "color12=#1F477F",
-                                   "--override",
-                                   "color13=#7B4474",
-                                   "--override",
-                                   "color14=#1B8486",
-                                   "--override",
-                                   "color15=#424242",
-                                   "--override",
-                                   "cursor=#FC9520",
-                                   NULL};
+// static const char* alttermcmd[] = {"kitty",
+//                                    "-1",
+//                                    "--instance-group",
+//                                    "Ysgrifennwr",
+//                                    "--override",
+//                                    "foreground=#424242",
+//                                    "--override",
+//                                    "background=#f9f8f4",
+//                                    "--override",
+//                                    "color0=#f9f8f4",
+//                                    "--override",
+//                                    "color1=#e32791",
+//                                    "--override",
+//                                    "color2=#488432",
+//                                    "--override",
+//                                    "color3=#a25d0e",
+//                                    "--override",
+//                                    "color4=#2c65b5",
+//                                    "--override",
+//                                    "color5=#B062A7",
+//                                    "--override",
+//                                    "color6=#27BBBE",
+//                                    "--override",
+//                                    "color7=#999999",
+//                                    "--override",
+//                                    "color8=#B8B8B8",
+//                                    "--override",
+//                                    "color9=#9F1B66",
+//                                    "--override",
+//                                    "color10=#325D23",
+//                                    "--override",
+//                                    "color11=#71410A",
+//                                    "--override",
+//                                    "color12=#1F477F",
+//                                    "--override",
+//                                    "color13=#7B4474",
+//                                    "--override",
+//                                    "color14=#1B8486",
+//                                    "--override",
+//                                    "color15=#424242",
+//                                    "--override",
+//                                    "cursor=#FC9520",
+//                                    NULL};
 // static const char* termcmd[] = {"urxvtc", "-name", "Qillqaq", NULL};
 static const char* emojicmd[] = {
     SHELL, "-c",
@@ -232,12 +250,12 @@ static const char* journalctlcmd[] = {
 static const char* abridorcmd[] = {"/home/xha/etc/suckless/abridor/abridor.lua",
                                    NULL};
 static const char* fm0cmd[] = {"/home/xha/etc/suckless/fm0/fm0.lua", NULL};
-static const char* sshaddcmd[] = {
-    SHELL, "-c",
-    "export SSH_ASKPASS=/usr/lib/ssh/x11-ssh-askpass\n[[ '256 "
-    "SHA256:1uG7O27tlFijVrOw9wW5taw+5lcNsS7ItGC4k+6doJI xaver@hellauer.bayern "
-    "(ED25519)' = `ssh-add -l` ]] || ssh-add < /dev/null",
-    NULL};
+// static const char* sshaddcmd[] = {
+// SHELL, "-c",
+// "export SSH_ASKPASS=/usr/lib/ssh/x11-ssh-askpass\n[[ '256 "
+// "SHA256:1uG7O27tlFijVrOw9wW5taw+5lcNsS7ItGC4k+6doJI xaver@hellauer.bayern "
+// "(ED25519)' = `ssh-add -l` ]] || ssh-add < /dev/null",
+// NULL};
 static const char* sshdelcmd[] = {"ssh-add", "-D", NULL};
 #define FONT_9MENU                                                             \
 	"-adobe-helvetica-medium-r-normal--14-140-75-75-p-77-iso10646-1"
@@ -304,36 +322,43 @@ static const char* main9menucmd[] = {
 static Key keys[] = {
     /* modifier  key  function  argument */
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
-    {MODKEY, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
+    {MODKEY, XK_Tab, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
-    {MODKEY, XK_s, incnmaster, {.i = +1}},
+    {MODKEY | ShiftMask, XK_Tab, focusstack, {.i = -1}},
+    {MODKEY, XK_i, incnmaster, {.i = +1}},
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_BackSpace, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY, XK_Tab, focusmaster, {0}},
+    {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY, XK_BackSpace, view, {0}},
     {MODKEY, XK_q, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[2]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_space, setlayout, {0}},
     {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_0, view, {.ui = 0xff}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = 0xff}},
     {MODKEY, XK_comma, focusmon, {.i = -1}},
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
-    {MODKEY, XK_ssharp, spawn, {.v = emojicmd}},
+    TAGKEYS(XK_0, 0),
+    TAGKEYS(XK_1, 1),
+    TAGKEYS(XK_2, 2),
+    TAGKEYS(XK_3, 3),
+    TAGKEYS(XK_4, 4),
+    TAGKEYS(XK_5, 5),
+    TAGKEYS(XK_6, 6),
+    TAGKEYS(XK_7, 7),
+    TAGKEYS(XK_8, 8),
+    TAGKEYS(XK_9, 9),
+    {MODKEY | ShiftMask, XK_q, quit, {0}},
+    {MODKEY, XK_e, spawn, {.v = emojicmd}},
     {MODKEY, XK_F1, spawn, {.v = mansplaincmd}},
-    {MODKEY, XK_i, spawn, {.v = clipcmd}},
-    {MODKEY, XK_acute, spawn, {.v = showclipboardcmd}},
+    {MODKEY, XK_Insert, spawn, {.v = clipcmd}},
+    {MODKEY, XK_s, spawn, {.v = showclipboardcmd}},
     {0, 0xff61, spawn, {.v = screenshotcmd}},
     {MODKEY, 0xff61, spawn, {.v = screenshotselectioncmd}},
     {0, 0x1008ff13, spawn, {.v = raisevolumecmd}},
@@ -363,15 +388,12 @@ static Key keys[] = {
     {0, 0x1008ff02, spawn, {.v = brightnessupcmd}},
     {0, 0x1008ff03, spawn, {.v = brightnessdowncmd}},
     {MODKEY | ShiftMask, XK_Escape, spawn, {.v = suspendcmd}},
-    {MODKEY, XK_F6, spawn, {.v = sshaddcmd}},
-    {MODKEY | ShiftMask, XK_F6, spawn, {.v = sshdelcmd}},
+    // {MODKEY, XK_F6, spawn, {.v = sshaddcmd}},
+    // {MODKEY | ShiftMask, XK_F6, spawn, {.v = sshdelcmd}},
     {MODKEY, XK_Escape, spawn, {.v = sshdelcmd}},
     {MODKEY, XK_Escape, spawn, {.v = lockcmd}},
-    {MODKEY | ShiftMask, XK_Return, spawn, {.v = alttermcmd}},
-    {MODKEY, XK_n, spawn, {.v = nnncmd}},
     {MODKEY, XK_o, spawn, {.v = abridorcmd}},
-    {MODKEY, XK_9, spawn, {.v = journalctlcmd}},
-    {MODKEY, XK_F3, tag, {.ui = 0x07}},
+    {MODKEY, XK_F7, spawn, {.v = journalctlcmd}},
     {MODKEY | ShiftMask, XK_F12, setlayout, {.v = &layouts[0]}},
     {MODKEY | ShiftMask, XK_F12, setmfact, {.f = 1.0f + DEFAULT_MFACT}},
     {MODKEY | ShiftMask, XK_F12, incnmaster, {.i = INT_MIN}},
@@ -402,14 +424,38 @@ static Button buttons[] = {
     {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
     {ClkClientWin, MODKEY, Button4, setmfact, {.f = +0.02}},
     {ClkClientWin, MODKEY, Button5, setmfact, {.f = -0.02}},
-    {ClkTagBar, 0, Button1, view, {0}},
-    {ClkTagBar, 0, Button2, toggleview, {0}},
-    {ClkTagBar, 0, Button3, tag, {0}}};
+    {ClkTagBar, 0, Button1, viewortoggleview, {0}},
+    {ClkTagBar, 0, Button2, toggletag, {0}},
+    {ClkTagBar, 0, Button3, view, {0}}};
 
 void focusmaster()
 {
 	Client* c = nexttiled(selmon->clients);
-	if (!selmon->sel || selmon->lt[selmon->sellt] != &layouts[0])
-		return;
+	if (!selmon->sel || selmon->lt[selmon->sellt] != &layouts[0]) return;
 	focus(c);
+}
+
+void viewortoggleview(const Arg* arg)
+{
+	unsigned int newtagset = 0U;
+	if (arg->ui & HOME_TAGS) {
+		if (selmon->tagset[selmon->seltags] & ~HOME_TAGS & TAGMASK) {
+			newtagset = HOME_TAGS & TAGMASK;
+		}
+		selmon->seltags ^= 1;
+	} else if (arg->ui & NONTOGGABLE_TAGS) {
+		newtagset =
+		    TAGMASK &
+		    (arg->ui ^ (HOME_TAGS | selmon->tagset[selmon->seltags] &
+		                                ~NONTOGGABLE_TAGS));
+		selmon->seltags ^= 1;
+	} else {
+		newtagset =
+		    selmon->tagset[selmon->seltags] ^ (arg->ui & TAGMASK) |
+		    HOME_TAGS;
+	}
+	if (newtagset) selmon->tagset[selmon->seltags] = newtagset;
+	focus(NULL);
+	arrange(selmon);
+	if (arg->ui & NONTOGGABLE_TAGS) focusmaster();
 }
