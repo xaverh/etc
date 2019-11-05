3865f063c80c96c1938e3a43bc7d42691549ee24
Xaver Hellauer <xaver@hellauer.bayern>
2019-11-05 20:37:39 +0100

    alacritty, changes to keybindings, VS Code theme switching via herbstluftwm

--- herbstluftwm/.config/herbstluftwm/panel.sh
+++ herbstluftwm/.config/herbstluftwm/panel.sh
@@ -10,10 +10,10 @@ fi
 x=${geometry[0]}
 y=${geometry[1]}
 panel_width=${geometry[2]}
-panel_height=18
+panel_height=20
 # bgcolor=$(hc get frame_border_normal_color)
 # selbg=$(hc get window_border_active_color)
 
 herbstclient pad $monitor $panel_height
 
-schwammerl $monitor 2> /dev/null | lemonbar -a 12 -g x${panel_height} -f -sgi-screen-medium-r-normal--12-120-72-72-m-70-iso8859-1 -f -xos4-terminus-medium-r-normal--12-120-72-72-c-60-iso10646-1 -B '#1e1e1e' -F '#e5e6e6' | /usr/bin/zsh
+schwammerl $monitor 2> /dev/null | lemonbar -a 12 -g x${panel_height} -f -sgi-screen-medium-r-normal--13-130-72-72-m-70-iso8859-1 -f -misc-fixed-medium-r-normal--13-120-75-75-c-70-iso10646-1 -B '#1e1e1e' -F '#e5e6e6' | /usr/bin/zsh
