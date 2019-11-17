a356b1160d11a8199c1ed5fbfac07219b8c1861a
Xaver Hellauer <xaver@hellauer.bayern>
2019-11-17 16:11:49 +0100

    bugfixing

--- herbstluftwm/.config/herbstluftwm/restartpanel.sh
+++ herbstluftwm/.config/herbstluftwm/restartpanel.sh
@@ -2,11 +2,11 @@
 
 pidof lemonbar && kill `pidof lemonbar`
 for monitor in $(herbstclient list_monitors | cut -d: -f1) ; do
-	local panel_height=18
+	local panel_height=16
 	geometry=( $(herbstclient monitor_rect "$monitor") )
 	x=${geometry[1]}
 	y=${geometry[2]}
 	width="${geometry[3]}"
-	(schwammerl "$monitor" | lemonbar -u 2 -g "$(printf '%dx%d%+d%+d' $width $panel_height $x $y)" -f -uw-ttyp0-medium-r-normal--14-130-75-75-c-70-iso10646-1 -f "-wenquanyi-wenquanyi bitmap song-medium-r-normal--12-120-75-75-p-119-iso10646-1" -a 13 -B "$QI_W" -F "$QI_B_K" | $SHELL ) &
+	(schwammerl "$monitor" | lemonbar -g "$(printf '%dx%d%+d%+d' $width $panel_height $x $y)" -f -uw-ttyp0-medium-r-normal--14-130-75-75-c-70-iso10646-1 -f "-wenquanyi-wenquanyi bitmap song-medium-r-normal--12-120-75-75-p-119-iso10646-1" -a 13 -B "$QI_W" -F "$QI_B_K" | $SHELL ) &
 	herbstclient pad "$monitor" "$panel_height"
 done
