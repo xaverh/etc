8a3570e02b97a47ada393bf7e22491905c2a224b
Xaver Hellauer <xaver.hellauer@gmail.com>
2020-02-19 22:21:36 +0100

    modeline test

--- dwm/.dwm/config.h
+++ dwm/.dwm/config.h
@@ -256,3 +256,5 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
+/* vim: setlocal ft=c: */
