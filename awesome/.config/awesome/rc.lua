85fc58d3202ffb24fb18ed8205f7454cf24d95dc
Xaver Hellauer <xaver.hellauer@gmail.com>
2020-02-22 03:47:17 +0100

    theming

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -17,6 +17,9 @@ local hotkeys_popup = require('awful.hotkeys_popup')
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require('awful.hotkeys_popup.keys')
+local theme_assets = require('beautiful.theme_assets')
+local xresources = require('beautiful.xresources')
+local dpi = xresources.apply_dpi
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -55,7 +58,11 @@ do
     )
 end
 
--- {{{ Variable definitions
+-- Options
+menubar.cache_entries = true
+menubar.show_categories = false
+
+-- Variable definitions
 
 local color_ys_w = 'f9f8f4' -- Floral White, R=249, G=248, B=244
 local color_ys_r = 'e32791' -- Deep Cerise, R=227, G=39, B=145
@@ -92,8 +99,7 @@ local color_ys_b_k = '424242' -- Grey 20%, R=66, G=66, B=66
 local color_qi_w_80 = '333333' -- Grey 20%, R=51, G=51, B=51
 local color_ys_w_80 = 'edece8' -- Grey 90%, R=237, G=236, B=232
 local color_cursor = '20bbfc' -- Deep Sky Blue, R=32, G=187, B=252
-
-local main_font = 'IBM Plex Sans:size=9'
+local assets = gears.filesystem.get_configuration_dir() .. 'assets/'
 
 local function increase_light_curry(brightness_file, max_brightness, emoji)
     local f = io.open(brightness_file, 'r')
@@ -123,8 +129,6 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { MAINFONT };
-static const char dmenufont[]       = MAINFONT;
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -132,8 +136,121 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { "#"QI_B_K, col_cyan,  "#"CURSOR_COLOR  },
 };
 ]]
+-- Theme
+
+local theme = {}
+
+theme.font = 'Helvetica 10'
+
+theme.bg_normal = '#' .. color_qi_w
+theme.bg_focus = '#005577'
+theme.bg_urgent = '#' .. color_qi_r
+theme.bg_minimize = '#444444'
+theme.bg_systray = theme.bg_normal
+
+theme.fg_normal = '#' .. color_ys_b_w
+theme.fg_focus = '#' .. color_qi_b_k
+theme.fg_urgent = '#' .. color_qi_b_k
+theme.fg_minimize = '#ffffff'
+
+theme.useless_gap = dpi(0)
+theme.border_width = dpi(2)
+theme.border_normal = '#' .. color_ys_b_k
+theme.border_focus = '#' .. color_cursor
+theme.border_marked = '#' .. color_qi_y
+
+-- There are other variable sets
+-- overriding the one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- prompt_[fg|bg|fg_cursor|bg_cursor|font]
+-- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
+
+-- Variables set for theming notifications:
+-- notification_font
+-- notification_[bg|fg]
+-- notification_[width|height|margin]
+-- notification_[border_color|border_width|shape|opacity]
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = assets .. 'submenu.png'
+theme.menu_height = dpi(15)
+theme.menu_width = dpi(100)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = assets .. 'titlebar/close_normal.png'
+theme.titlebar_close_button_focus = assets .. 'titlebar/close_focus.png'
+
+theme.titlebar_minimize_button_normal = assets .. 'titlebar/minimize_normal.png'
+theme.titlebar_minimize_button_focus = assets .. 'titlebar/minimize_focus.png'
+
+theme.titlebar_ontop_button_normal_inactive = assets .. 'titlebar/ontop_normal_inactive.png'
+theme.titlebar_ontop_button_focus_inactive = assets .. 'titlebar/ontop_focus_inactive.png'
+theme.titlebar_ontop_button_normal_active = assets .. 'titlebar/ontop_normal_active.png'
+theme.titlebar_ontop_button_focus_active = assets .. 'titlebar/ontop_focus_active.png'
+
+theme.titlebar_sticky_button_normal_inactive = assets .. 'titlebar/sticky_normal_inactive.png'
+theme.titlebar_sticky_button_focus_inactive = assets .. 'titlebar/sticky_focus_inactive.png'
+theme.titlebar_sticky_button_normal_active = assets .. 'titlebar/sticky_normal_active.png'
+theme.titlebar_sticky_button_focus_active = assets .. 'titlebar/sticky_focus_active.png'
+
+theme.titlebar_floating_button_normal_inactive = assets .. 'titlebar/floating_normal_inactive.png'
+theme.titlebar_floating_button_focus_inactive = assets .. 'titlebar/floating_focus_inactive.png'
+theme.titlebar_floating_button_normal_active = assets .. 'titlebar/floating_normal_active.png'
+theme.titlebar_floating_button_focus_active = assets .. 'titlebar/floating_focus_active.png'
+
+theme.titlebar_maximized_button_normal_inactive = assets .. 'titlebar/maximized_normal_inactive.png'
+theme.titlebar_maximized_button_focus_inactive = assets .. 'titlebar/maximized_focus_inactive.png'
+theme.titlebar_maximized_button_normal_active = assets .. 'titlebar/maximized_normal_active.png'
+theme.titlebar_maximized_button_focus_active = assets .. 'titlebar/maximized_focus_active.png'
+
+theme.wallpaper = gears.filesystem.get_xdg_cache_home() .. 'Tapet/2020-02-14_17-05-44_883_1920x1200.png'
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = assets .. 'layouts/fairh.png'
+theme.layout_fairv = assets .. 'layouts/fairv.png'
+theme.layout_floating = assets .. 'layouts/floating.png'
+theme.layout_magnifier = assets .. 'layouts/magnifier.png'
+theme.layout_max = assets .. 'layouts/max.png'
+theme.layout_fullscreen = assets .. 'layouts/fullscreen.png'
+theme.layout_tilebottom = assets .. 'layouts/tilebottom.png'
+theme.layout_tileleft = assets .. 'layouts/tileleft.png'
+theme.layout_tile = assets .. 'layouts/tile.png'
+theme.layout_tiletop = assets .. 'layouts/tiletop.png'
+theme.layout_spiral = assets .. 'layouts/spiral.png'
+theme.layout_dwindle = assets .. 'layouts/dwindle.png'
+theme.layout_cornernw = assets .. 'layouts/cornernw.png'
+theme.layout_cornerne = assets .. 'layouts/cornerne.png'
+theme.layout_cornersw = assets .. 'layouts/cornersw.png'
+theme.layout_cornerse = assets .. 'layouts/cornerse.png'
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. 'default/theme.lua')
+beautiful.init(theme)
 
 -- This is used later as the default terminal and editor to run.
 terminal = os.getenv('TERMINAL') or 'alacritty'
@@ -348,7 +465,7 @@ local function set_wallpaper(s)
         if type(wallpaper) == 'function' then
             wallpaper = wallpaper(s)
         end
-        gears.wallpaper.maximized(wallpaper, s, true)
+        gears.wallpaper.maximized(wallpaper, s, false)
     end
 end
 
