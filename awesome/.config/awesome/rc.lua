00c1580475156ade3069b18fa40683192bafc56c
Xaver Hellauer <xaver.hellauer@gmail.com>
2020-02-24 01:53:22 +0100

    clean dependencies

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1,22 +1,11 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, 'luarocks.loader')
-
--- Standard awesome library
 local gears = require 'gears'
 local awful = require 'awful'
 require 'awful.autofocus'
--- Widget and layout library
 local wibox = require 'wibox'
--- Theme handling library
 local beautiful = require 'beautiful'
--- Notification library
 local naughty = require 'naughty'
 local menubar = require 'menubar'
 local hotkeys_popup = require 'awful.hotkeys_popup'
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
--- require('awful.hotkeys_popup.keys')
 local theme_assets = require 'beautiful.theme_assets'
 local xresources = require 'beautiful.xresources'
 local dpi = xresources.apply_dpi
