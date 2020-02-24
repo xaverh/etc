df0c9cf80b26115255d7dae0e8c6df28f67d7923
Xaver Hellauer <xaver.hellauer@gmail.com>
2020-02-24 01:56:52 +0100

    cleanup

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -10,35 +10,8 @@ local theme_assets = require 'beautiful.theme_assets'
 local xresources = require 'beautiful.xresources'
 local dpi = xresources.apply_dpi
 
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal(
-        'debug::error',
-        function(err)
-            -- Make sure we don't go into an endless error loop
-            if in_error then
-                return
-            end
-            in_error = true
-
-            naughty.notify(
-                {
-                    preset = naughty.config.presets.critical,
-                    title = 'Oops, an error happened!',
-                    text = tostring(err)
-                }
-            )
-            in_error = false
-        end
-    )
-end
-
--- Options
 menubar.cache_entries = true
 menubar.show_categories = false
-
--- Variable definitions
 
 local colors = {
     qi = {
