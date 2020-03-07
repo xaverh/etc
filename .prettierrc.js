c2d4b0f0c7a29486eaf7e975d9684c9bd3af437a
Xaver Hellauer <xaver.hellauer@gmail.com>
2020-03-08 00:28:07 +0100

    exceptions for Markdown

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,5 +2,14 @@ module.exports = {
 	tabWidth: 4,
 	semi: false,
 	singleQuote: true,
-	useTabs: true
+	useTabs: true,
+	overrides: [
+		{
+			files: ['*.md', '*.markdown'],
+			options: {
+				tabWidth: 2,
+				useTabs: false
+			}
+		}
+	]
 }
