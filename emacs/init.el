ec3a3067cae578c63f05f14e498df60c37a9ecc6
Xaver Hellauer <xaver.hellauer@gmail.com>
2021-08-07 17:42:52 +0200

    🐃 Emacs

---
+++ emacs/init.el
@@ -0,0 +1,24 @@
+
+(setq-default cursor-type 'bar)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(inhibit-startup-screen t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "screen" :foundry "sgi" :slant normal :weight normal :height 130 :width normal)))))
+ ;; '(default ((t (:family "SF Mono" :slang normal :weight normal :height 120 :width normal)))))
+
