d434d211b0f9c552ef6153e3103157952b2f29d3
Xaver Hellauer <xaver.hellauer@gmail.com>
2021-08-08 18:55:28 +0200

    Update Emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -6,12 +6,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scroll-bar-mode nil)
- '(show-paren-mode t)
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
+ '(indicate-empty-lines t)
  '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
+ '(show-paren-mode t)
  '(tool-bar-mode nil))
 
 (custom-set-faces
@@ -19,6 +20,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "screen" :foundry "sgi" :slant normal :weight normal :height 130 :width normal)))))
- ;; '(default ((t (:family "SF Mono" :slang normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "SF Mono" :slang normal :weight normal :height 120 :width normal)))))
 
