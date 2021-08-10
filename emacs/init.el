8c67b3832445f962687cadd811e1dff2945a2803
Xaver Hellauer <xaver.hellauer@gmail.com>
2021-08-10 23:27:55 +0200

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,5 @@
 
-(setq-default cursor-type 'bar)
+;; (setq-default cursor-type 'bar)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -8,11 +8,16 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
+ '(fringe-mode 0 nil (fringe))
+ '(global-display-line-numbers-mode t)
  '(indicate-empty-lines t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
+ '(save-place-mode t)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tab-bar-mode t)
  '(tool-bar-mode nil))
 
 (custom-set-faces
@@ -20,5 +25,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "SF Mono" :slang normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "SF Mono" :foundry "APPL" :slant normal :weight normal :height 121 :width normal)))))
+
+
+(set-fontset-font t 'symbol "JoyPixels")
+
 
