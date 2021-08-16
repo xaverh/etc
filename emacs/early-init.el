285a797a1d24a4a5205c4cd97344ca0a078abd38
Xaver Hellauer <xaver.hellauer@gmail.com>
2021-08-16 22:07:13 +0200

    Add files via upload

---
+++ emacs/early-init.el
@@ -0,0 +1,21 @@
+;; -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
+ '(global-display-line-numbers-mode t)
+ '(global-tab-line-mode t)
+ '(initial-scratch-message nil)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tooltip-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(visible-bell t))
+
+(setq inhibit-splash-screen t)
+
+
