e32cf59d29a4220712aef7d7aab7f0b97b7ce352
Xaver Hellauer <xaver.hellauer@gmail.com>
2021-10-02 13:28:31 +0200

    Add files via upload

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -5,15 +5,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(cua-mode t nil (cua-base))
+ '(fringe-mode 36 nil (fringe))
  '(global-display-line-numbers-mode t)
  '(global-tab-line-mode t)
+ '(indicate-buffer-boundaries nil)
+ '(indicate-empty-lines nil)
+ '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(menu-bar-mode nil)
+ '(save-place-mode t)
  '(scroll-bar-mode nil)
- '(tooltip-mode nil)
  '(show-paren-mode t)
+ '(size-indication-mode t)
  '(tool-bar-mode nil)
+ '(tooltip-mode nil)
  '(visible-bell t))
 
 (setq inhibit-splash-screen t)
--- emacs/init.el
+++ emacs/init.el
@@ -1,62 +1,42 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Disable line numbers in some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Be resilient of missing packages.
+(defmacro require-soft (name &rest body)
+  `(if (require ,name nil t)
+       (progn ,@body)
+     (message "Could not load \"%s\", skipping..." ,name)))
+
+;; (setq-default cursor-type 'bar)
+
+(set-fontset-font t 'symbol "OpenMoji Color")
+
+; (set-window-margins (selected-window) 3 3)
+
+(require-soft 'evil)
+(evil-mode 1)
+
+(require-soft 'ivy)
+(ivy-mode 1)
+
+(require-soft 'rainbow-delimiters
+	      (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "PragmataPro Liga" :foundry "FSD " :slant normal :weight normal :height 128 :width normal))))
- '(show-paren-match ((((class color)) (:foreground "green"))))
- '(show-paren-mismatch ((((class color)) (:foreground "red")))))
+ '(default ((t (:family "SF Mono" :foundry "APPL" :slant normal :weight normal :height 113 :width normal)))))
+
 (put 'scroll-left 'disabled nil)
 
-(load-theme 'tango-dark)
-
-;; (setq package-archive '(("melpa" . "https://melpa.org/packages/")
-;; 			("org" . "https://orgmode.org/elpa/")
-;; 			("elpa" . "https://elpa.gnu.org/packages/")))
-
-;; (package-initialize)
-;; (unless package-archive-contents
-;;   (package-refresh-contents))
-
-;; ;; Initialize use-package on non-Linux platforms
-;; (unless (package-installed-p 'use-package)
-;;   (package-install 'use-package))
-
-;; (require 'use-package)
-;; (setq use-package-always-ensure t)
-
-
-
-
-(setq-default cursor-type 'bar)
-
-;; below is the old stuff
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(cua-mode t nil (cua-base))
- '(fringe-mode 0 nil (fringe))
- '(global-display-line-numbers-mode t)
- '(indicate-empty-lines t)
- '(inhibit-startup-screen t)
- '(menu-bar-mode nil)
- '(save-place-mode t)
- '(scroll-bar-mode nil)
- '(show-paren-mode t)
- '(size-indication-mode t)
- '(tool-bar-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "SF Mono" :foundry "APPL" :slant normal :weight normal :height 121 :width normal)))))
-
-(set-fontset-font t 'symbol "JoyPixels")
